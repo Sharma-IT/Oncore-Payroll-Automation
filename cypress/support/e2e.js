b7e2e9f0a4ef91e8c4a181c6edaee8ab66bef3f2
Shubham Sharma <shubhamsharma.emails@gmail.com>
2024-09-21 14:04:26 +0000

    update: e2e.js

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,7 +1,6 @@
 import './commands'
 
 Cypress.on('uncaught:exception', (err, runnable) => {
-    console.error('Uncaught Exception:', err.message);
-  
-    return false;
-  });
+  console.error('Uncaught Exception:', err.message);
+  return false;
+});
